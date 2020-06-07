1045213f161e5abb1e3cbf4378ab30b7b1f675f4
Sun-Mountain <nzonnenberg@fearless.tech>
2020-06-07 15:30:37 -0400

    Revert back to rosters

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,7 +1,7 @@
 class Title < ApplicationRecord
   belongs_to :user
-  has_many :session_rosters, dependent: :destroy
-  has_many :block_sessions, through: :session_rosters
+  has_many :rosters, dependent: :destroy
+  has_many :block_sessions, through: :rosters
 
   validates :class_title, presence: true
 
