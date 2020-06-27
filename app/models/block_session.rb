696e390853f6937b820d80e08989d7c4b20e8286
Sun-Mountain <nzonnenberg@fearless.tech>
2020-06-27 16:11:28 -0400

    Sessions CRUD complete

--- app/models/block_session.rb
+++ app/models/block_session.rb
@@ -1,8 +1,2 @@
 class BlockSession < ApplicationRecord
-  has_many :rosters, dependent: :destroy
-  has_many :titles, through: :rosters
-
-  def classes?
-    rosters.count.zero?
-  end
 end
--- app/models/title.rb
+++ app/models/title.rb
@@ -1,11 +1,6 @@
 class Title < ApplicationRecord
   belongs_to :user
-  has_many :rosters, dependent: :destroy
-  has_many :block_sessions, through: :rosters
 
   validates :class_title, presence: true
 
-  def sessions?
-    rosters.count.zero?
-  end
 end
